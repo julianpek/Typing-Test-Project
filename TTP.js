53c88dbe8db1c31d29e6f10b873cc92ca60ba51c
chefavocado <85806647+chefavocado@users.noreply.github.com>
2021-11-13 21:41:06 -0500

    Countdown

--- TTP.js
+++ TTP.js
@@ -1,13 +1,26 @@
 const TEST_WORDS = ["apples", "the", "or", "welcome", "Canada", "zebra", "wave", "kitchen", "snacks", "laptop", "coffee", "purple", "because", "who", "with", "a", "music", "quote", "business", "cola", "come", "went", "dragons", "yes", "jump", "how", "dance", "ox", "Batman", "memes", "yeet", "bananas", "snake", "paper", "too", "new", "orange", "view", "summer", "picture", "out", "ghost", "thanks", "last", "beer", "algorithm", "function", "script", "wood", "bag", "cheetos", "tooth", "refer", "knit", "hero", "gene", "yum", "exercise", "heavy", "pizza", "happy", "eye", "young", "blood", "scary", "nut", "no", "at", "hi", "my"];
 // shuffles array of words
 const TEST_WORDS_SHUFFLED = TEST_WORDS.sort((a, b) => 0.5 - Math.random());
+const TIMER_EL = document.getElementById("countdown");
+let seconds = 60;
 
-//  displays shuffled words dynamically
+// displays shuffled words dynamically
 function shuffledWords() {
     let prg = document.getElementById("shuff-words");
     prg.innerHTML = TEST_WORDS_SHUFFLED.join(" ");
 }
-
+// refreshes page
 function refresh() {
-    window.location.reload("Refresh");
+    window.location.reload();
 }
+// timer
+setInterval(function() {
+    seconds--;
+
+    if(seconds >= 0) {
+        id = document.getElementById("countdown");
+        id.innerHTML = `${seconds}`;
+    }
+}, 1000);
+
+ 
