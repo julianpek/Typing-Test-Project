3cf4916389965e4c919045bc5517801f835e6a6d
chefavocado <85806647+chefavocado@users.noreply.github.com>
2021-11-29 22:05:52 -0500

    Big time updatez

--- TTP.js
+++ TTP.js
@@ -1,11 +1,9 @@
 const TEST_WORDS = ["apples", "the", "or", "welcome", "Canada", "zebra", "wave", "kitchen", "snacks", "laptop", "coffee", "purple", "because", "who", "with", "a", "music", "quote", "business", "cola", "come", "went", "dragons", "yes", "jump", "how", "dance", "ox", "Batman", "memes", "yeet", "bananas", "snake", "paper", "too", "new", "orange", "view", "summer", "picture", "out", "ghost", "thanks", "last", "beer", "algorithm", "function", "script", "wood", "bag", "cheetos", "tooth", "refer", "knit", "hero", "gene", "yum", "exercise", "heavy", "pizza", "happy", "eye", "young", "blood", "scary", "nut", "no", "at", "hi", "my"];
 // shuffles array of words
 const TEST_WORDS_SHUFFLED = TEST_WORDS.sort((a, b) => 0.5 - Math.random());
-const TIMER_EL = document.getElementById("countdown");
-let wordsDiv = document.getElementById("words-div");
 let countdown = false;
 let current_position = 0;
-let seconds = 60;
+let seconds = 10;
 
 // populates words and places each individual character into a span
 function words() {
@@ -22,41 +20,7 @@ function words() {
     div.firstElementChild.id = "cursor";
 }
 
-// refresh page
-function refresh() {
-    window.location.reload();
-}
-// timer and stats for type test
-function testTimer() {
-
-    setInterval(function() {
-        seconds--;
-
-        if(seconds >= 0) {
-            id = document.getElementById("countdown");
-            id.innerHTML = `${seconds}`;
-        // } else { 
-        //     let correct = document.querySelectorAll(".correct").length;
-        //     let incorrect = document.querySelectorAll(".incorrect").length; 
-        //     let total = correct + incorrect;
-        //     let results = correct / total * 100;
-        //     let accuracy = "Accuracy: " + results.toFixed() + "%";
-        //     let wpmresults = total / 5 - incorrect / seconds;
-        //     let wpm = "WPM: " + wpmresults.toFixed();
-        //     let wordsDiv = document.getElementById("words-div");
-
-        //     wordsDiv.style.visibility = "hidden"
-        //     alert(wpm + " " + accuracy);
-        }
-    }, 1000);
-    countdown = true;
-}    
-
-function startCountdown() {
-    if (!countdown) testTimer();
-}
-
-//calculates stats
+//calculates stats, hides content-container, and displays stats-container once timer expires
 function stats() {
     let correct = document.querySelectorAll(".correct").length;
     let incorrect = document.querySelectorAll(".incorrect").length; 
@@ -65,9 +29,45 @@ function stats() {
     let accuracy = "Accuracy: " + accuracyResults.toFixed() + "%";
     let wpmResults = total / 5 - incorrect / seconds;
     let wpm = "WPM: " + wpmResults.toFixed();
+    let wpmEl = document.getElementById("wpm");
+    let accuracyEl = document.getElementById("accuracy");
+    let timer = document.getElementById("countdown");
+    let timerCon = timer.innerText;
+    let wordsCon = document.getElementById("contents-container");
+    let statsCon = document.getElementById("stats-container");
 
-    // if (wordsDiv.style.visibility = "hidden"
-    // wpm + " " + accuracy);
+
+    if(timerCon === "0") {
+        wpmEl.innerText = wpm;
+        accuracyEl.innerText = accuracy; 
+
+        wordsCon.style.visibility = "hidden";
+        statsCon.style.visibility = "visible";
+    }
+}
+
+// refresh page
+function refresh() {
+    window.location.reload();
+}
+
+// timer and stats for type test
+function testTimer() {
+    setInterval(function() {
+        seconds--;
+
+        if(seconds >= 0) {
+            id = document.getElementById("countdown");
+            id.innerHTML = `${seconds}`;
+        } else {
+            stats();
+        }
+    }, 1000);
+    countdown = true;
+}    
+
+function startCountdown() {
+    if (!countdown) testTimer();
 }
 
 //event listeners
